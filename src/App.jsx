a8b9e7bfc1d5aada9007c6909904d1f1aee1312e
Abhay Pal <abhaypal1298@gmail.com>
2024-12-26 18:57:55 +0530

    sixth commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
-
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <>
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
